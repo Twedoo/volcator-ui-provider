89bb9a1424bf2330c118fcc01c5fc319a409c98f
unknown <ben.hmida.fares@gmail.com>
2023-06-12 15:12:15 +0200

    add insert many command

--- src/main/java/com/twedoo/volcator/infrastructure/web/CommandResource.java
+++ src/main/java/com/twedoo/volcator/infrastructure/web/CommandResource.java
@@ -3,6 +3,7 @@ package com.twedoo.volcator.infrastructure.web;
 import com.mongodb.BasicDBObject;
 import com.mongodb.client.result.DeleteResult;
 import com.mongodb.client.result.UpdateResult;
+import com.twedoo.volcator.infrastructure.helper.Command;
 import com.twedoo.volcator.infrastructure.helper.RequestCommand;
 import io.quarkus.mongodb.reactive.ReactiveMongoClient;
 import io.quarkus.mongodb.reactive.ReactiveMongoCollection;
@@ -13,6 +14,9 @@ import jakarta.ws.rs.core.MediaType;
 import org.bson.Document;
 import org.bson.types.ObjectId;
 import org.eclipse.microprofile.config.inject.ConfigProperty;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Path("/command")
 @Produces(MediaType.APPLICATION_JSON)
@@ -36,7 +40,24 @@ public class CommandResource {
                  )
                  .onItem().ignore().andContinueWithNull();
     }
+    @POST
+    @Path("add-list-command/{collectionName}")
+    public Uni<Void> addListCommand(List<Command> commands, String collectionName) {
+        List<Document> documents = new ArrayList<>();
 
+        for (Command command : commands) {
+            Document document = new Document()
+                    .append("label", command.getLabel())
+                    .append("value", command.getValue());
+
+            documents.add(document);
+        }
+
+        return getCollection(collectionName)
+                .insertMany(
+                        documents
+                ).onItem().ignore().andContinueWithNull();
+    }
     @PUT
     @Path("edit-command")
     public Uni<UpdateResult> editCommand(RequestCommand requestCommand) {
